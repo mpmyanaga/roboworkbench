e44d77e002c758706a4295013722920b3f967fc2
dan@dancowan.co.uk <dan@dancowan.co.uk@60abb826-fd37-11de-9a2d-79d510f670ec>
2010-01-16 23:42:37 +0000

    Browser fixes for correct URL and browser type

--- uk.co.dancowan.robots.srv/src/uk/co/dancowan/robots/srv/ui/views/wcs/WCSView.java
+++ uk.co.dancowan.robots.srv/src/uk/co/dancowan/robots/srv/ui/views/wcs/WCSView.java
@@ -13,21 +13,13 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package uk.co.dancowan.robots.srv.ui.views.wcs;
 
-import java.util.logging.Logger;
-
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.SWTException;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
-import uk.co.dancowan.robots.hal.logger.LoggingService;
 import uk.co.dancowan.robots.srv.SRVConfig;
-import uk.co.dancowan.robots.ui.utils.ColourManager;
 import uk.co.dancowan.robots.ui.views.ScrolledView;
 
 /**
@@ -40,9 +32,9 @@ public class WCSView extends ScrolledView
 {
 	public static final String ID = "robots.srv.WCSView";
 
-	private static final Logger ERROR_LOGGER = Logger.getLogger(LoggingService.ERROR_LOGGER);
 	private static final Point MIN_SIZE = new Point(300, 140);
 
+	@Override
 	public String getID()
 	{
 		return ID;
@@ -51,31 +43,13 @@ public class WCSView extends ScrolledView
 	/**
 	 * @see uk.co.dancowan.robots.ui.views.ScrolledView#getPartControl(org.eclipse.swt.widgets.Composite)
 	 */
+	@Override
 	public Control getPartControl(Composite parent)
 	{
-		final Composite part = new Composite(parent, SWT.BORDER);
-		part.setBackground(ColourManager.getColour(ColourManager.ERROR_COLOUR));
-		part.setLayout(new FormLayout());
-
-		try
-		{
-			final Browser browser = new Browser(part, SWT.MOZILLA);
-			String server = SRVConfig.sProperties.get("wcs.server");
-			String port = SRVConfig.sProperties.get("wcs.port");
-			browser.setUrl(server + ":" + port);
-
-			FormData data = new FormData();
-			data.top = new FormAttachment(0, 6);
-			data.left = new FormAttachment(0, 6);
-			data.right = new FormAttachment(100, -6);
-			data.bottom = new FormAttachment(100, -6);
-			browser.setLayoutData(data);
-		}
-		catch(SWTException e)
-		{
-			ERROR_LOGGER.finest(e.getMessage());
-		}
-
+		Browser part = new Browser(parent, SWT.BORDER);
+		String server = SRVConfig.sProperties.get("wcs.server");
+		String port = SRVConfig.sProperties.get("wcs.port");
+		part.setUrl("http://" + server + ":" + port);
 		part.pack();
 		return part;
 	}
