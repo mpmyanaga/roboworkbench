855558b7357809166fa396a8a5bbf8c8da177f90
dan@dancowan.co.uk <dan@dancowan.co.uk@60abb826-fd37-11de-9a2d-79d510f670ec>
2010-01-19 00:37:04 +0000

    Extensive changes for edit, drawing, updating, listening

--- trunk/uk.co.dancowan.robots.srv/src/uk/co/dancowan/robots/srv/ui/panels/PatternWidget.java
+++ trunk/uk.co.dancowan.robots.srv/src/uk/co/dancowan/robots/srv/ui/panels/PatternWidget.java
@@ -18,6 +18,8 @@ import org.eclipse.jface.preference.PreferenceConverter;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
@@ -26,8 +28,10 @@ import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
 
 import uk.co.dancowan.robots.srv.SRVActivator;
+import uk.co.dancowan.robots.srv.hal.SrvHal;
 import uk.co.dancowan.robots.srv.hal.featuredetector.Pattern;
 import uk.co.dancowan.robots.srv.hal.featuredetector.PatternListener;
 import uk.co.dancowan.robots.srv.ui.preferences.PreferenceConstants;
@@ -38,26 +42,69 @@ import uk.co.dancowan.robots.ui.utils.ColourManager;
  * 
  * @author Dan Cowan
  */
-public class PatternWidget extends Composite implements PaintListener, IPropertyChangeListener, PatternListener
+public class PatternWidget extends Composite implements PaintListener, IPropertyChangeListener, PatternListener, FeatureWidget
 {
 	private final Pattern mPattern;
+	private final boolean mGrid;
 
 	private Color mForeground;
 	private Color mBackground;
+	private Color mGridColour;
+
+	private boolean mSelected;
+	private boolean mInvert;
+
+	private int mXIndent;
+	private int mYIndent;
+	private int mUnit;
 
 	/**
 	 * C'tor
 	 * 
 	 * @param pattern the Pattern instance to render
 	 */
-	public PatternWidget(Composite parent, Pattern pattern)
+	public PatternWidget(Composite parent, Pattern pattern, boolean paintGrid)
 	{
 		super(parent, SWT.BORDER);
 
-		addPaintListener(this);
 		mPattern = pattern;
 		mPattern.addListener(this);
+		mGrid = paintGrid;
+		mSelected = false;
+		mInvert = false;
+
+		addMouseListener(new MouseAdapter()
+		{
+			@Override
+			public void mouseDown(MouseEvent e)
+			{
+				selectWidget();
+			}
+		});
+
+		addPaintListener(this);
 		initFromPrefs();
+	}
+
+	public void selectWidget()
+	{
+		if ( ! mSelected)
+		{
+			mSelected = true;
+			SrvHal.getCamera().getDetector().setFocusPattern(mPattern.getIndex());
+			redraw();
+			notifyListeners(SWT.Selection, new Event()); // event is not read so OK just as a token
+		}
+	}
+
+	/**
+	 * Returns this widget's <code>Pattern</code>.
+	 * 
+	 * @return Pattern
+	 */
+	public Pattern getPattern()
+	{
+		return mPattern;
 	}
 
 	/**
@@ -69,25 +116,68 @@ public class PatternWidget extends Composite implements PaintListener, IProperty
 		Point size = getSize();
 		int square = size.x > size.y ? size.y : size.x;
 		square -= 2;
-		int unit = square / 8;
+		mUnit = square / 8;
+		mXIndent = (size.x-square)/2;
+		mYIndent = (size.y-square)/2;
+
+		Color foreground = mInvert ? mBackground : mForeground;
+		Color background = mInvert ? mForeground : mBackground;
 
 		GC graphics = e.gc;
-		graphics.setBackground(mBackground);
+		if (mSelected && ! mGrid)
+			graphics.setBackground(ColourManager.getColour(SWT.COLOR_WHITE));
+		else
+			graphics.setBackground(background);
 		graphics.fillRectangle(0, 0, size.x, size.y);
-		graphics.setBackground(mForeground);
+		graphics.setBackground(foreground);
 		for (int i = 0; i < 8; i ++)
 		{
 			String str = mPattern.get(i);
 			assert str.length() == 8;
 			for (int j = 0; j < 8; j ++)
 			{
-				int x = (size.x-square)/2 + j*unit;
-				int y = (size.y-square)/2 + i*unit;
+				int x = mXIndent + j*mUnit;
+				int y = mYIndent/2 + i*mUnit;
 				if (str.charAt(j) == '1')
-					graphics.fillRectangle(x, y, unit, unit);
+					graphics.fillRectangle(x, y, mUnit, mUnit);
+
+				if (mGrid)
+				{
+					graphics.setForeground(mGridColour);
+					graphics.drawPolygon(new int[]{x, y, x + mUnit, y, x + mUnit, y + mUnit, x, y + mUnit});
+				}
 			}
 		}
-		System.err.println("");
+	}
+
+	/**
+	 * Returns the X-axis indent of the left edge of the pattern of the composite in pixels.
+	 * 
+	 * @return int
+	 */
+	protected int getXIndent()
+	{
+		return mXIndent;
+	}
+
+	/**
+	 * Returns the Y-axis indent of the left edge of the pattern of the composite in pixels.
+	 * 
+	 * @return int
+	 */
+	protected int getYIndent()
+	{
+		return mYIndent;
+	}
+
+	/**
+	 * Returns the unit size of pattern element in pixels.
+	 * 
+	 * @return int
+	 */
+	protected int getUnit()
+	{
+		return mUnit;
 	}
 
 	/**
@@ -99,13 +189,16 @@ public class PatternWidget extends Composite implements PaintListener, IProperty
 		if (event.getProperty().equals(PreferenceConstants.PATTERN_COLOUR))
 		{
 			mForeground = ColourManager.getInstance().getColour((RGB) event.getNewValue());
-			redraw();
 		}
-		else if (event.getProperty().equals(PreferenceConstants.CAMERA_BACKGROUND_COLOUR))
+		else if (event.getProperty().equals(PreferenceConstants.PATTERN_BACKGROUND_COLOUR))
 		{
 			mBackground = ColourManager.getInstance().getColour((RGB) event.getNewValue());
-			redraw();
 		}
+		else if (event.getProperty().equals(PreferenceConstants.PATTERN_EDITOR_GRID_COLOUR))
+		{
+			mGridColour = ColourManager.getInstance().getColour((RGB) event.getNewValue());
+		}
+		redraw();
 	}
 
 	/**
@@ -128,6 +221,60 @@ public class PatternWidget extends Composite implements PaintListener, IProperty
 		});
 	}
 
+	/**
+	 * Returns if this widget is selected.
+	 * 
+	 * @see uk.co.dancowan.robots.srv.ui.panels.FeatureWidget#getSelection()
+	 */
+	@Override
+	public boolean getSelection()
+	{
+		return mSelected;
+	}
+
+	/**
+	 * Sets the selection state of this widget.
+	 * 
+	 * @see uk.co.dancowan.robots.srv.ui.panels.FeatureWidget#setSelection(boolean)
+	 */
+	@Override
+	public void setSelection(boolean state)
+	{
+		mSelected = state;
+	}
+
+	public void matched()
+	{
+		Thread thread = new Thread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				for (int i = 0; i < 10; i ++)
+				{
+					Display.getDefault().asyncExec(new Runnable()
+					{
+						@Override
+						public void run()
+						{
+							mInvert =  ! mInvert;
+							redraw();
+						}
+					});
+					try
+					{
+						Thread.sleep(200);
+					}
+					catch (InterruptedException e)
+					{
+						// NOP
+					}
+				}
+			}
+		});
+		thread.start();
+	}
+
 	/*
 	 * Set initial behaviour from plugin preferences
 	 * Add this class as a change listener
@@ -136,8 +283,10 @@ public class PatternWidget extends Composite implements PaintListener, IProperty
 	{
 		IPreferenceStore store = SRVActivator.getDefault().getPreferenceStore();
 		mForeground = ColourManager.getInstance().getColour(PreferenceConverter.getColor(store, PreferenceConstants.PATTERN_COLOUR));
-		mBackground = ColourManager.getInstance().getColour(PreferenceConverter.getColor(store, PreferenceConstants.CAMERA_BACKGROUND_COLOUR));
+		mBackground = ColourManager.getInstance().getColour(PreferenceConverter.getColor(store, PreferenceConstants.PATTERN_BACKGROUND_COLOUR));
+		mGridColour = ColourManager.getInstance().getColour(PreferenceConverter.getColor(store, PreferenceConstants.PATTERN_EDITOR_GRID_COLOUR));
 
 		store.addPropertyChangeListener(this);
 	}
+
 }
