3af1578fa11fcce1032eccc1fc2915a0aaedf905
dan@dancowan.co.uk <dan@dancowan.co.uk@60abb826-fd37-11de-9a2d-79d510f670ec>
2010-02-10 21:02:54 +0000

    Basic edit menu added. CoolBar items added.

--- uk.co.dancowan.robots.ui/src/uk/co/dancowan/robots/ui/ApplicationActionBarAdvisor.java
+++ uk.co.dancowan.robots.ui/src/uk/co/dancowan/robots/ui/ApplicationActionBarAdvisor.java
@@ -14,15 +14,24 @@
 package uk.co.dancowan.robots.ui;
 
 import org.eclipse.jface.action.GroupMarker;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.ICoolBarManager;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.action.ToolBarContributionItem;
+import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.swt.SWT;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
+
+import uk.co.dancowan.robots.ui.editors.actions.NewFileAction;
+import uk.co.dancowan.robots.ui.editors.actions.OpenFileAction;
 
 /**
  * Eclipse foundation class to create the action bar.
@@ -35,10 +44,20 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor
 {
 	private IWorkbenchAction mExitAction;
 	private IWorkbenchAction mPreferencesAction;
-	//private IWorkbenchAction mAboutAction;
+	private IWorkbenchAction mAboutAction;
 	private IWorkbenchAction mHelpAction;
 	private IWorkbenchAction mHelpSearchAction;
 	private IWorkbenchAction mHelpDynamicAction;
+
+	private IAction mNewAction;
+	private IAction mOpenAction;
+	private IAction mSaveAction;
+	private IAction mSaveAsAction;
+	private IAction mCutAction;
+	private IAction mCopyAction;
+	private IAction mPasteAction;
+	//private IAction mUndoAction;
+	//private IAction mRedoAction;
 
 	/**
 	 * C'tor.
@@ -63,8 +82,8 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor
 		mExitAction = ActionFactory.QUIT.create(window);
 		register(mExitAction);
 
-		//mAboutAction = ActionFactory.ABOUT.create(window);
-		//register(mAboutAction);
+		mAboutAction = ActionFactory.ABOUT.create(window);
+		register(mAboutAction);
 
 		mPreferencesAction = ActionFactory.PREFERENCES.create(window);
 		register(mPreferencesAction);
@@ -77,6 +96,25 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor
 
 		mHelpDynamicAction = ActionFactory.DYNAMIC_HELP.create(window);
 		register(mHelpDynamicAction);
+
+		mNewAction = new NewFileAction();
+
+		mOpenAction = new OpenFileAction();
+
+		mSaveAction = ActionFactory.SAVE.create(window);
+		register(mSaveAction);
+
+		mSaveAsAction = ActionFactory.SAVE_AS.create(window);
+		register(mSaveAsAction);
+
+		mCutAction = ActionFactory.CUT.create(window);
+		register(mCutAction);
+
+		mCopyAction = ActionFactory.COPY.create(window);
+		register(mCopyAction);
+
+		mPasteAction = ActionFactory.PASTE.create(window);
+		register(mPasteAction);
 	}
 
 	/**
@@ -85,24 +123,67 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor
 	protected void fillMenuBar(IMenuManager menuBar)
 	{
 		MenuManager fileMenu = new MenuManager("&File", IWorkbenchActionConstants.M_FILE);
+		MenuManager editMenu = new MenuManager("&Edit", IWorkbenchActionConstants.M_EDIT);
 		MenuManager windowMenu = new MenuManager("&Window", IWorkbenchActionConstants.M_WINDOW);
 		MenuManager helpMenu = new MenuManager("&Help", IWorkbenchActionConstants.M_HELP);
-		menuBar.add(fileMenu);
-		menuBar.add(windowMenu);
-		menuBar.add(helpMenu);
-		
+
+		fileMenu.add(new GroupMarker(IWorkbenchActionConstants.FILE_START));		
+		fileMenu.add(mNewAction);		
+		fileMenu.add(mOpenAction);		
+		fileMenu.add(mSaveAction);		
+		fileMenu.add(mSaveAsAction);
+		fileMenu.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
+		fileMenu.add(new Separator());
 		fileMenu.add(mExitAction);
-		
+		fileMenu.add(new GroupMarker(IWorkbenchActionConstants.FILE_END));
+
+		editMenu.add(new GroupMarker(IWorkbenchActionConstants.EDIT_START));		
+		editMenu.add(mCutAction);		
+		editMenu.add(mCopyAction);		
+		editMenu.add(mPasteAction);
+		editMenu.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
+		editMenu.add(new GroupMarker(IWorkbenchActionConstants.EDIT_END));
+
 		windowMenu.add(new GroupMarker("defaultParts"));
 		windowMenu.add(new Separator());
 		windowMenu.add(new GroupMarker("robotExtensions"));
 		windowMenu.add(new Separator());
+		windowMenu.add(new GroupMarker(IWorkbenchActionConstants.WINDOW_EXT));		
 		windowMenu.add(mPreferencesAction);
 		
-		//helpMenu.add(mAboutAction);
+		helpMenu.add(new GroupMarker(IWorkbenchActionConstants.HELP_START));
+		helpMenu.add(mAboutAction);
 		helpMenu.add(new Separator());
 		helpMenu.add(mHelpAction);
 		helpMenu.add(mHelpSearchAction);
 		helpMenu.add(mHelpDynamicAction);
+		helpMenu.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
+		helpMenu.add(new GroupMarker(IWorkbenchActionConstants.HELP_END));
+
+		menuBar.add(fileMenu);
+		menuBar.add(editMenu);
+		menuBar.add(windowMenu);
+		menuBar.add(helpMenu);
+	}
+
+	/**
+	 * @see org.eclipse.ui.application.ActionBarAdvisor#fillCoolBar(org.eclipse.jface.action.ICoolBarManager)
+	 */
+	protected void fillCoolBar(ICoolBarManager coolBar)
+	{
+		IToolBarManager fileBar = new ToolBarManager(SWT.FLAT | SWT.LEFT);
+		IToolBarManager editBar = new ToolBarManager(SWT.FLAT | SWT.LEFT);
+
+		fileBar.add(mNewAction);
+		fileBar.add(mOpenAction);
+		fileBar.add(mSaveAction);
+		fileBar.add(mSaveAsAction);
+
+		editBar.add(mCutAction);
+		editBar.add(mCopyAction);
+		editBar.add(mPasteAction);
+
+		coolBar.add(new ToolBarContributionItem(fileBar, "file"));
+		coolBar.add(new ToolBarContributionItem(editBar, "edit"));
 	}
 }
