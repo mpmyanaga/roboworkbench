3043a738e49bc39dd8ee7565f7aea5001a057759
dan@dancowan.co.uk <dan@dancowan.co.uk@60abb826-fd37-11de-9a2d-79d510f670ec>
2010-02-10 22:31:52 +0000

    Initial commit. First look at a custom tokeniser and syntax highlighting

---
+++ uk.co.dancowan.robots.srv/src/uk/co/dancowan/robots/srv/editors/PicoCEditor.java
@@ -0,0 +1,64 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  Copyright (c) 2009 Dan Cowan
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details (www.gnu.org/licenses)
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package uk.co.dancowan.robots.srv.editors;
+
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+
+import uk.co.dancowan.robots.srv.SRVActivator;
+import uk.co.dancowan.robots.ui.editors.BasicTextEditor;
+
+/**
+ * Class adds picoC tokenising and highlighting for BasicTextEditor
+ * 
+ * @author Dan Cowan
+ * @since version 1.0.0
+ */
+public class PicoCEditor extends BasicTextEditor
+{
+	private Model mModel;
+
+	/**
+	 * Sets the passed text string as the editor's content
+	 * 
+	 * @param text
+	 */
+	@Override
+	public void setText(String text)
+	{
+		super.setText(text);
+		if (mModel != null)
+			mModel.setText(text);
+	}
+
+	/**
+	 * @see org.eclipse.ui.IWorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	public void createPartControl(Composite parent)
+	{
+		super.createPartControl(parent);
+		mModel = new Model(getText());
+		setText(getText().getText()); // bit of a hack to reset the text once the model is created
+	}
+
+	/**
+	 * @see org.eclipse.ui.IWorkbenchPart#getTitleImage()
+	 */
+	@Override
+	public Image getTitleImage()
+	{
+		return SRVActivator.getImageDescriptor("icons/editor_picoC.gif").createImage();
+	}
+}
