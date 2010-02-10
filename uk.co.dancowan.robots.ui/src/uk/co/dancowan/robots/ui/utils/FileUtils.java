77c06e402f3e45b2ed3c38feffdcf0d2c7eaa7c0
dan@dancowan.co.uk <dan@dancowan.co.uk@60abb826-fd37-11de-9a2d-79d510f670ec>
2010-02-10 21:08:10 +0000

    Initial commit. Method to find an registered editor for a file type and open it.

---
+++ uk.co.dancowan.robots.ui/src/uk/co/dancowan/robots/ui/utils/FileUtils.java
@@ -0,0 +1,41 @@
+package uk.co.dancowan.robots.ui.utils;
+
+import java.io.File;
+
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorRegistry;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PartInitException;
+
+import uk.co.dancowan.robots.ui.Activator;
+import uk.co.dancowan.robots.ui.editors.FileEditorInput;
+
+public class FileUtils
+{
+	public static void openFile(File file)
+	{
+		boolean external = false;
+		try
+		{
+			IWorkbench workbench = Activator.getDefault().getWorkbench();
+			IEditorDescriptor editor = workbench.getEditorRegistry().getDefaultEditor(file.getName());
+			if (editor == null)
+			{
+				external = true;
+				editor = workbench.getEditorRegistry().findEditor(IEditorRegistry.SYSTEM_EXTERNAL_EDITOR_ID);
+			}
+			if (editor != null)
+			{
+				String editorId = editor.getId();
+				IEditorInput input = new FileEditorInput(file);
+				workbench.getActiveWorkbenchWindow().getActivePage().openEditor(input, editorId);
+			}
+		}
+		catch (PartInitException e)
+		{
+			String message = external ? "" : "No editor could be found to handle this file.";
+			Activator.hanldeError("Editor Error", message);
+		}
+	}
+}
