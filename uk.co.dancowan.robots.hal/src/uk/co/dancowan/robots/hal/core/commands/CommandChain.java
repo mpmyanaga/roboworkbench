440b2ecac6699f01e9cb1c19a4e92560961771e8
dan@dancowan.co.uk <dan@dancowan.co.uk@60abb826-fd37-11de-9a2d-79d510f670ec>
2010-02-10 21:21:20 +0000

    Initial commit. Command runs a configured sequence of commands serially

---
+++ uk.co.dancowan.robots.hal/src/uk/co/dancowan/robots/hal/core/commands/CommandChain.java
@@ -0,0 +1,48 @@
+package uk.co.dancowan.robots.hal.core.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import uk.co.dancowan.robots.hal.core.Command;
+import uk.co.dancowan.robots.hal.core.CommandQ;
+
+public class CommandChain extends AbstractCommand
+{
+	private List<Command> mCommands;
+
+	public CommandChain()
+	{
+		super(true);
+		mCommands = new ArrayList<Command>();
+	}
+
+	public void clearCommands()
+	{
+		mCommands.clear();
+	}
+
+	public void addCommand(Command cmd)
+	{
+		mCommands.add(cmd);
+	}
+
+	public void removeCommand(Command cmd)
+	{
+		mCommands.remove(cmd);
+	}
+
+	public void execute(CommandQ cmdQ)
+	{
+		for (Command cmd : mCommands)
+		{
+			cmd.execute(cmdQ);
+			new PauseQCmd(100).execute(cmdQ);
+		}
+	}
+
+	@Override
+	protected String getCommandString()
+	{
+		throw new UnsupportedOperationException("CommandChain cannot return a command string.");
+	}
+}
