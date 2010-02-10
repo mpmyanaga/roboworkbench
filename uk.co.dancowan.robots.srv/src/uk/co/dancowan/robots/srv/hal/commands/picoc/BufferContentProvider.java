ebb7c540ffddfc65fe910daacce26faac22172c9
dan@dancowan.co.uk <dan@dancowan.co.uk@60abb826-fd37-11de-9a2d-79d510f670ec>
2010-02-10 22:41:00 +0000

    Initial commit. PicoC transmition and execution commands

---
+++ uk.co.dancowan.robots.srv/src/uk/co/dancowan/robots/srv/hal/commands/picoc/BufferContentProvider.java
@@ -0,0 +1,30 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  Copyright (c) 2009 Dan Cowan
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details (www.gnu.org/licenses)
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package uk.co.dancowan.robots.srv.hal.commands.picoc;
+
+/**
+ * Interface marks classes capable of providing text to send to the SRV1 flash buffer
+ * 
+ * @author Dan Cowan
+ * @since version 1.0.0
+ */
+public interface BufferContentProvider
+{
+	/**
+	 * Return the content for the buffer
+	 * 
+	 * @return String
+	 */
+	public String getText();
+}
