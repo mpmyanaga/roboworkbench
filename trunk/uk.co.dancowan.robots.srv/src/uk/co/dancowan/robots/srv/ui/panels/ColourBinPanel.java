b74debdd73f6d22dadfd66da021e53faee555d9f
dan@dancowan.co.uk <dan@dancowan.co.uk@60abb826-fd37-11de-9a2d-79d510f670ec>
2010-05-01 20:27:32 +0000

    Refactor for Panel upgrade

--- trunk/uk.co.dancowan.robots.srv/src/uk/co/dancowan/robots/srv/ui/panels/ColourBinPanel.java
+++ trunk/uk.co.dancowan.robots.srv/src/uk/co/dancowan/robots/srv/ui/panels/ColourBinPanel.java
@@ -13,6 +13,8 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package uk.co.dancowan.robots.srv.ui.panels;
 
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -21,6 +23,10 @@ import org.eclipse.swt.widgets.Group;
 
 import uk.co.dancowan.robots.srv.hal.featuredetector.FeatureDetector;
 import uk.co.dancowan.robots.srv.ui.views.camera.CameraCanvas;
+import uk.co.dancowan.robots.srv.ui.views.featuredetector.actions.BlobOverlayAction;
+import uk.co.dancowan.robots.srv.ui.views.featuredetector.actions.RefreshBinsAction;
+import uk.co.dancowan.robots.srv.ui.views.featuredetector.actions.RefreshBlobsAction;
+import uk.co.dancowan.robots.ui.views.Panel;
 
 /**
  * Class manages a view sub-panel for the low level colour vision in the SRV1.
@@ -34,6 +40,8 @@ public class ColourBinPanel implements Panel
 
 	private final FeatureWidgetRadioManager mBinSet;
 	private final CameraCanvas mCameraCanvas;
+
+	private Group mColourBinComposite;
 
 	/**
 	 * C'tor
@@ -50,7 +58,7 @@ public class ColourBinPanel implements Panel
 	}
 
 	/**
-	 * @see uk.co.dancowan.robots.srv.ui.panels.Panel#getID()
+	 * @see uk.co.dancowan.robots.ui.views.Panel#getID()
 	 */
 	@Override
 	public String getID()
@@ -59,33 +67,51 @@ public class ColourBinPanel implements Panel
 	}
 
 	/**
-	 * @see uk.co.dancowan.robots.srv.ui.panels.Panel#getPanel(org.eclipse.swt.widgets.Composite)
+	 * @see uk.co.dancowan.robots.ui.views.Panel#getDescription()
+	 */
+	public String getDescription()
+	{
+		return "Colour Bin Panel";
+	}
+
+	/**
+	 * @see uk.co.dancowan.robots.ui.views.Panel#getPanel(org.eclipse.swt.widgets.Composite)
 	 */
 	@Override
 	public Composite getPanel(Composite parent)
 	{
-		Group panel = new Group(parent, SWT.NONE);
-		panel.setText("Colour Bins");
-		panel.setLayout(new GridLayout(FeatureDetector.BIN_COUNT, true));
+		mColourBinComposite = new Group(parent, SWT.NONE);
+		mColourBinComposite.setText("Colour Bins");
+		mColourBinComposite.setLayout(new GridLayout(FeatureDetector.BIN_COUNT*2, true));
 
 		final ColourBinEditor editor = new ColourBinEditor(mCameraCanvas);
 		for (int i = 0; i < FeatureDetector.BIN_COUNT; i ++)
 		{
-			ColourBinWidget c = new ColourBinWidget(i, panel, editor);
-			c.setLayoutData(new GridData(GridData.FILL_BOTH));
+			ColourBinWidget c = new ColourBinWidget(i, mColourBinComposite, editor);
+			GridData gd = new GridData(GridData.FILL_BOTH);
+			gd.horizontalSpan = 2;
+			c.setLayoutData(gd);
 			if (i == 0)
 				c.setSelection(true);
 			mBinSet.addWidget(c);
 		}
 		
-		editor.addWidgets(panel);
-		panel.pack();
+		editor.addWidgets(mColourBinComposite);
+		mColourBinComposite.pack();
 		editor.selectionChanged();
-		return panel;
+		return mColourBinComposite;
+	}
+
+	public void addToToolBar(IToolBarManager manager)
+	{
+		manager.appendToGroup("actions", new RefreshBinsAction(mColourBinComposite));
+    	manager.appendToGroup("actions", new RefreshBlobsAction());
+    	manager.appendToGroup("actions", new Separator());
+    	manager.appendToGroup("actions", new BlobOverlayAction(mCameraCanvas));
 	}
 
 	/**
-	 * @see uk.co.dancowan.robots.srv.ui.panels.Panel#dispose()
+	 * @see uk.co.dancowan.robots.ui.views.Panel#dispose()
 	 */
 	public void dispose()
 	{
